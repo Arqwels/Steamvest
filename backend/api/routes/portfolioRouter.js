4fa422dff1469d81ab88fd80fe6538430e81f543
Arqwels <danil.kuzmin.1994@gmail.com>
2025-10-11 20:40:02 +0400

    добавить middleware checkPortfolioOwnership для роутов портфолио

--- backend/api/routes/portfolioRouter.js
+++ backend/api/routes/portfolioRouter.js
@@ -1,29 +1,24 @@
 const { Router } = require('express');
 const portfolioController = require('../controllers/portfolioController');
+const checkPortfolioOwnership = require('../middlewares/checkPortfolioOwnership');
 const router = Router();
 
 // Создание портфолио
-// POST http://localhost:5000/api/portfolio
-router.post('/', portfolioController.createPortfolio);
+router.post('/', checkPortfolioOwnership, portfolioController.createPortfolio);
 
 // Список всех портфолио
-// GET http://localhost:5000/api/portfolio
 router.get('/', portfolioController.getAllPortfolios);
 
 // Получить активное портфолио
-// GET http://localhost:5000/api/portfolio/active
-router.get('/active', portfolioController.getActivePortfolio);
+router.get('/active', checkPortfolioOwnership, portfolioController.getActivePortfolio);
 
 // Установить портфолио активным
-// PATCH http://localhost:5000/api/portfolio/:id/activate
-router.patch('/:id/activate', portfolioController.activatePortfolio);
+router.patch('/:portfolioId/activate', checkPortfolioOwnership, portfolioController.activatePortfolio);
 
 // Переименовать портфолио
-// PATCH http://localhost:5000/api/portfolio/:id
-router.patch('/:id', portfolioController.renamePortfolio);
+router.patch('/:portfolioId', checkPortfolioOwnership, portfolioController.renamePortfolio);
 
 // Удаление портфолио
-// DELETE http://localhost:5000/api/portfolio/:id
-router.delete('/:id', portfolioController.deletePortfolio);
+router.delete('/:portfolioId', checkPortfolioOwnership, portfolioController.deletePortfolio);
 
 module.exports = router;
